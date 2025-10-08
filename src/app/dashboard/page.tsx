21c658cfff0ad2af6569053a777fd1f997ce5ac4
Underprotection <mertcandemirkaya@outlook.com>
2025-10-08 14:34:42 +0300

    08: Dashboard Page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,17 @@
+import { ActivityOverview } from "@/components/dashboard/activity-overview";
+import { MainActions } from "@/components/dashboard/main-actions";
+import { WelcomeSection } from "@/components/dashboard/welcome-section";
 import { Navbar } from "@/components/navbar";
 
 export default function Dashboard() {
   return (
     <div>
       <Navbar />
-      Dashboard
+      <div className="max-w-7xl mx-auto px-6 py-8 pt-24">
+        <WelcomeSection />
+        <MainActions />
+        <ActivityOverview />
+      </div>
     </div>
   );
 }
