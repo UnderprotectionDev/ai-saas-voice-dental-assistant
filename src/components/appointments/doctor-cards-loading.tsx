6057a777d818a80f2b91186f1bf97b3d47a03ce7
Underprotection <mertcandemirkaya@outlook.com>
2025-10-09 18:19:23 +0300

    uncomment

--- src/components/appointments/doctor-cards-loading.tsx
+++ src/components/appointments/doctor-cards-loading.tsx
@@ -34,7 +34,6 @@ function DoctorCardSkeleton() {
 }
 
 export function DoctorCardsLoading() {
-  // this will show 6 skeleton cards
   return (
     <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
       {Array.from({ length: 6 }).map((_, i) => (
